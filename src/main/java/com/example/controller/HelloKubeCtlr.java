7c19fe9f4ca20c9f6401b158b7fe3db70f0c6e91
esakk <esakk@192.168.0.20>
2020-12-29 13:26:32 -0500

    hello

---
+++ src/main/java/com/example/controller/HelloKubeCtlr.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloKubeCtlr {
+
+	@GetMapping("/hello-kube")
+	public String returnHello() {
+		return "Hello from Kubernetes";
+	}
+}
